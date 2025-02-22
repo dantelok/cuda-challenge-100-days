2b9e25603ef76904e2d54a4dacbf9af9e010f81d
Dante Lok <lo4855@hotmail.com>
2025-02-23 02:41:41 +0800

    day6 challenge

--- day05/dot_product_shared_memory.cu
+++ day05/dot_product_shared_memory.cu
@@ -1,12 +1,98 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 
-__global__ void dot_product_shared_memory_reduction(int n) {
-    __shared__ double a, b, c;
+#define N 10000
+#define BLOCK_SIZE 256
 
+
+__global__ void dot_product_shared_memory_reduction(double *a, double *b, double *partial_sums, int n) {
+    __shared__ double partial_sum[BLOCK_SIZE];
+
+    // id for global memory, containing multiple blocks
     int id = blockDim.x * blockIdx.x + threadIdx.x;
 
+    // Thread index for shared memory, only 1 block
+    int tid = threadIdx.x;
+
     if (id < n) {
-        c[id] += a[id] * b[id];
+        partial_sum[tid] = a[id] * b[id];
+    } else {
+        partial_sum[tid] = 0.0;
     }
+    __syncthreads(); // ensures that all threads reach the same point before continuing
+    // Used in shared memory reduction, ensuring correct execution order
+
+    // Reduction in shared memory (tree-based reduction)
+    for (int stride = BLOCK_SIZE / 2; stride > 0; stride /= 2) {
+        if (tid < stride) {
+            partial_sum[tid] += partial_sum[tid + stride];
+        }
+        __syncthreads();
+    }
+
+    // Write the results back to global memory from shared memory
+    if (tid == 0) {
+        partial_sums[blockIdx.x] = partial_sum[0];
+    }
+}
+
+void init_vector(double *vector, int n) {
+    for (int i = 0; i < n; i++) {
+        vector[i] = (double)rand() / RAND_MAX;
+    }
+}
+
+int main() {
+    double *host_a, *host_b, *host_partial_sum;
+    double *device_a, *device_b, *device_partial_sum;
+
+    // Get num_blocks
+    int num_blocks = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    // Get size
+    size_t size = N * sizeof(double);
+
+    // Allocate memory on host
+    host_a = (double*)malloc(size);
+    host_b = (double*)malloc(size);
+    host_partial_sum = (double*)malloc(num_blocks * sizeof(double));
+
+    // Allocate memory on device
+    cudaMalloc(&device_a, size); 
+    cudaMalloc(&device_b, size); 
+    cudaMalloc(&device_partial_sum, num_blocks * sizeof(double)); 
+
+    // Init vector
+    init_vector(host_a, N);
+    init_vector(host_b, N);
+
+    // Copy from host to device
+    cudaMemcpy(device_a, host_a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_b, host_b, size, cudaMemcpyHostToDevice);
+
+    // Run the kernel
+    dot_product_shared_memory_reduction<<<num_blocks, BLOCK_SIZE>>>(device_a, device_b, device_partial_sum, N);
+    cudaDeviceSynchronize();
+
+    // Copy results
+    cudaMemcpy(host_partial_sum, device_partial_sum, num_blocks * sizeof(double), cudaMemcpyDeviceToHost);
+
+    // Sum the reduction up in host
+    double total_sum = 0.0;
+    for (int i = 0; i < num_blocks; i ++) {
+        total_sum += host_partial_sum[i];
+    }
+
+    // Verify Results
+    printf("Total Sum = %f\n", total_sum);
+
+    // Free Memory
+    free(host_a);
+    free(host_b);
+    free(host_partial_sum);
+    cudaFree(device_a);
+    cudaFree(device_b);
+    cudaFree(device_partial_sum);
+    
+    return 0;
 }
---
+++ day06/matrix_vector_multiplication.cu
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+#define N 2048
+#define M 2048
+#define BLOCK_SIZE 256
+
+// Matrix A: (m, n) dot vector x: (n, 1) -> vector y: (m, 1)
+__global__ void matrix_vector_multiplcation(double *A, double *x, double *y, int m, int n) {
+
+    // threads = row
+    int row = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (row < m) {
+        double sum = 0.0;
+        for (int col = 0; col < n; col++) {
+            // the number of elements = no. of elements per row * row + col
+            sum += A[n * row + col] * x[col];
+        }
+        y[row] = sum;
+    }
+}
+
+void init_vector(double *vector, int n) {
+    for (int i = 0; i < n; i++) {
+        vector[i] = (double)rand() / RAND_MAX;
+    }
+}
+
+void init_matrix(double *matrix, int m, int n) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++){
+            matrix[i * n + j] = (double)rand() / RAND_MAX;
+        }
+    }
+}
+
+int main() {
+    double *host_A, *host_x, *host_y;
+    double *device_A, *device_x, *device_y;
+
+    // Get num_blocks
+    int num_blocks = (M + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    // Get size
+    size_t size_A = M * N * sizeof(double);
+    size_t size_x = N * sizeof(double);
+    size_t size_y = M * sizeof(double);
+
+    // Allocate memory on host
+    host_A = (double*)malloc(size_A);
+    host_x = (double*)malloc(size_x);
+    host_y = (double*)malloc(size_y);
+
+    // Allocate memory on device
+    cudaMalloc(&device_A, size_A); 
+    cudaMalloc(&device_x, size_x); 
+    cudaMalloc(&device_y, size_y); 
+
+    // Init vector
+    init_matrix(host_A, M, N);
+    init_vector(host_x, N);
+
+    // Copy from host to device
+    cudaMemcpy(device_A, host_A, size_A, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_x, host_x, size_x, cudaMemcpyHostToDevice);
+
+    // Run the kernel
+    matrix_vector_multiplcation<<<num_blocks, BLOCK_SIZE>>>(device_A, device_x, device_y, M, N);
+    cudaDeviceSynchronize();
+
+    // Copy results
+    cudaMemcpy(host_y, device_y, size_y, cudaMemcpyDeviceToHost);
+
+    // Verify Results
+    for (int i = 0; i < M; i++) {
+        printf("y[%d] = %f\n", i, host_y[i]);
+    }
+
+    // Free Memory
+    free(host_A);
+    free(host_x);
+    free(host_y);
+    cudaFree(device_A);
+    cudaFree(device_x);
+    cudaFree(device_y);
+    
+    return 0;
+}
